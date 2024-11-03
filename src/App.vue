2a9cda1117b4db9397e4ec8814f8741da7640536
PrometheusOn <ramazanfajzov@mail.ru>
2024-11-04 02:03:50 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import PayFormPage from './components/PayFormPage.vue'
+</script>
+
+<template>
+  <PayFormPage initiatorName="Файзов Рамазан" duesTitle="Тестовый" />
+</template>
+
+<style scoped></style>
