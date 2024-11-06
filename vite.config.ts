acb1b0e7abd1ebc592c836c413656136d1d9fe3a
PrometheusOn <ramazanfajzov@mail.ru>
2024-11-06 12:42:13 +0500

    #6 фикс ошибок

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
-import { URL, fileURLToPath } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { URL, fileURLToPath } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-   base: '/PAYMO/',
+  base: '/PAYMO/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
