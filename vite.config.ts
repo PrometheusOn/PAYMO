de1a09c6ab1831b1e24a6f42a9242ae6534a50c4
PrometheusOn <ramazanfajzov@mail.ru>
2024-11-06 13:07:34 +0500

    defineConfig base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { URL, fileURLToPath } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/PAYMO/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
