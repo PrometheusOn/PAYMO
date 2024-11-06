4ebd7bd478f75a73cc96c4b58572217c6692af21
PrometheusOn <ramazanfajzov@mail.ru>
2024-11-06 15:06:11 +0500

    #test

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { URL, fileURLToPath } from 'node:url'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: '/PAYMO/',
